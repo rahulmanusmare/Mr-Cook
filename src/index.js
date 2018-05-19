61370e1fe160bf0a58db4fb632eeea803f72adbe
errorishere <iamrahul7798@gmail.com>
2018-05-19 10:31:52 +0530

    VUI Design
    Added Interaction model in Speeh Assets for Voice user interface

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const SKILL_NAME = 'Mr Cook';
+const HELP_MESSAGE = 'You can say tell me a something to eat, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
 var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback) {
@@ -13,10 +18,26 @@ var handlers = {
     },
 
     'LaunchIntent': function() {
-        this.emit(':ask', "Hello how are you?");
+        this.emit(':ask', "Hello how can i help you?");
     },
 
     'HelloIntent': function() {
         this.emit(':tell', "Good to hear from you. You have successfully tested hello world skill. Good bye.");
     }
+
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = HELP_MESSAGE;
+        const reprompt = HELP_REPROMPT;
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
+    'AMAZON.StopIntent': function () {
+        this.response.speak(STOP_MESSAGE);
+        this.emit(':responseReady');
+    },
 };
